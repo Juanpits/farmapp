535f696afd8a04e027416663ebf9f521987ae6f7
Juan Pablo Delgado Salcedo <juanpablofds@gmail.com>
2021-10-30 12:09:01 -0500

    Se construyeron los informes del reto

--- src/main/java/Farm/Reto3/ControladorReservaciones.java
+++ src/main/java/Farm/Reto3/ControladorReservaciones.java
@@ -26,13 +26,15 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/api/Reservation")
-@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT,RequestMethod.DELETE})
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 
 public class ControladorReservaciones {
-      @Autowired
+
+    @Autowired
     private ServiciosReservaciones servicio;
+
     @GetMapping("/all")
-    public List<Reservaciones> getReservations(){
+    public List<Reservaciones> getReservations() {
         return servicio.getAll();
     }
 
@@ -40,19 +42,37 @@ public class ControladorReservaciones {
     public Optional<Reservaciones> getReservation(@PathVariable("id") int reservationId) {
         return servicio.getReservation(reservationId);
     }
+
     @PostMapping("/save")
     @ResponseStatus(HttpStatus.CREATED)
     public Reservaciones save(@RequestBody Reservaciones reservation) {
         return servicio.save(reservation);
     }
+
     @PutMapping("/update")
     @ResponseStatus(HttpStatus.CREATED)
     public Reservaciones update(@RequestBody Reservaciones reservation) {
         return servicio.update(reservation);
     }
+
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public boolean delete(@PathVariable("id") int reservationId) {
         return servicio.deleteReservation(reservationId);
+    }
+
+    @GetMapping("/report-status")
+    public StatusReservas getReservas() {
+        return servicio.reporteStatusServicio();
+    }
+
+    @GetMapping("/report-dates/{dateOne}/{dateTwo}")
+    public List<Reservaciones> getReservasTiempo(@PathVariable("dateOne") String dateOne, @PathVariable("dateTwo") String dateTwo) {
+        return servicio.reporteTiempoServicio(dateOne, dateTwo);
+    }
+
+    @GetMapping("/report-clients")
+    public List<ContadorClientes> getClientes() {
+        return servicio.reporteClientesServicio();
     }
 }
--- src/main/java/Farm/Reto3/ServiciosReservaciones.java
+++ src/main/java/Farm/Reto3/ServiciosReservaciones.java
@@ -4,6 +4,10 @@
  */
 package Farm.Reto3;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,10 +19,11 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class ServiciosReservaciones {
-     @Autowired
+
+    @Autowired
     private RepositorioReservaciones metodosCrud;
 
-    public List<Reservaciones> getAll(){
+    public List<Reservaciones> getAll() {
         return metodosCrud.getAll();
     }
 
@@ -26,39 +31,39 @@ public class ServiciosReservaciones {
         return metodosCrud.getReservation(reservationId);
     }
 
-    public Reservaciones save(Reservaciones reservation){
-        if(reservation.getIdReservation()==null){
+    public Reservaciones save(Reservaciones reservation) {
+        if (reservation.getIdReservation() == null) {
             return metodosCrud.save(reservation);
-        }else{
-            Optional<Reservaciones> e= metodosCrud.getReservation(reservation.getIdReservation());
-            if(e.isEmpty()){
+        } else {
+            Optional<Reservaciones> e = metodosCrud.getReservation(reservation.getIdReservation());
+            if (e.isEmpty()) {
                 return metodosCrud.save(reservation);
-            }else{
+            } else {
                 return reservation;
             }
         }
     }
 
-    public Reservaciones update(Reservaciones reservation){
-        if(reservation.getIdReservation()!=null){
-            Optional<Reservaciones> e= metodosCrud.getReservation(reservation.getIdReservation());
-            if(!e.isEmpty()){
+    public Reservaciones update(Reservaciones reservation) {
+        if (reservation.getIdReservation() != null) {
+            Optional<Reservaciones> e = metodosCrud.getReservation(reservation.getIdReservation());
+            if (!e.isEmpty()) {
 
-                if(reservation.getStartDate()!=null){
+                if (reservation.getStartDate() != null) {
                     e.get().setStartDate(reservation.getStartDate());
                 }
-                if(reservation.getDevolutionDate()!=null){
+                if (reservation.getDevolutionDate() != null) {
                     e.get().setDevolutionDate(reservation.getDevolutionDate());
                 }
-                if(reservation.getStatus()!=null){
+                if (reservation.getStatus() != null) {
                     e.get().setStatus(reservation.getStatus());
                 }
                 metodosCrud.save(e.get());
                 return e.get();
-            }else{
+            } else {
                 return reservation;
             }
-        }else{
+        } else {
             return reservation;
         }
     }
@@ -69,5 +74,36 @@ public class ServiciosReservaciones {
             return true;
         }).orElse(false);
         return aBoolean;
+    }
+
+    public StatusReservas reporteStatusServicio() {
+        List<Reservaciones> completed = metodosCrud.ReservacionStatusRepositorio("completed");
+        List<Reservaciones> cancelled = metodosCrud.ReservacionStatusRepositorio("cancelled");
+
+        return new StatusReservas(completed.size(), cancelled.size());
+    }
+
+    public List<Reservaciones> reporteTiempoServicio(String datoA, String datoB) {
+        SimpleDateFormat parser = new SimpleDateFormat("yyyy-MM-dd");
+
+        Date datoUno = new Date();
+        Date datoDos = new Date();
+
+        try {
+            datoUno = parser.parse(datoA);
+            datoDos = parser.parse(datoB);
+        } catch (ParseException evt) {
+            evt.printStackTrace();
+        }
+        if (datoUno.before(datoDos)) {
+            return metodosCrud.ReservacionTiempoRepositorio(datoUno, datoDos);
+        } else {
+            return new ArrayList<>();
+
+        }
+    }
+
+    public List<ContadorClientes> reporteClientesServicio() {
+        return metodosCrud.getClientesRepositorio();
     }
 }
